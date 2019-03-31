6d2d8673abce1e1c2103d995d1b13267842d9876
HamzaCharko <48889435+HamzaCharko@users.noreply.github.com>
2019-03-31 01:27:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import discord
 from discord.ext import commands
+
 client = discord.Client()
 
 def __init__(self, bot):
@@ -223,4 +224,4 @@ def setup(bot):
     bot.add_cog(Music(bot))
     print('Music is loaded')
 
-client.run('BOT_TOKEN')
+client.run(str(os.environ.get('BOT_TOKEN')))
